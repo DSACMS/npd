c322c19d455d7b61da53fc61fc17b32a3d9528ce
Isaac Milarsky <imilarsky@gmail.com>
2025-09-11 16:20:11 -0500

    Add fhir checks file with some schema checks using pydantic and some nucc code checks from the db
    Signed-off-by: Isaac Milarsky <imilarsky@gmail.com>

---
+++ src/npdfhir/fhir_checks.py
@@ -0,0 +1,100 @@
+import traceback
+from fhir.resources.valueset import ValueSet
+from pydantic import ValidationError
+from django.db import connection
+
+
+def parse_nucc_codes_into_dicts(db_result):
+    codes = {}
+    for nucc_code_record in db_result:
+        codes[nucc_code_record[0]] = nucc_code_record[1]
+    
+    return codes
+
+def get_nucc_codes_from_db():
+    query = """
+        SELECT nucc_code, display_name, nucc_grouping_id
+        FROM npd.nucc_classification;
+    """
+
+    with connection.cursor() as cursor:
+        cursor.execute(query)
+        results = cursor.fetchall()
+
+    return parse_nucc_codes_into_dicts(results)
+
+class BaseVerifier:
+    """
+        Class that uses pydantic to verify the FHIR schema as
+        pulled from the fhir.resources package.
+
+        fhir_resource_type : attribute that determines the fhir schema to validate.
+    """
+    def __init__(self,fhir_obj):
+        self.fhir_resource_type = fhir_obj
+
+    def fhir_resource_validate_schema(self,data):
+        #Use pydantic and fhir resources to validate the schema
+        try:
+            _ = self.fhir_resource_type.model_validate_json(data)
+        except ValidationError as e:
+            print(f"Data was found that violates {self.fhir_resource_type.__name__} schema:")
+            print(f"Data: {data}")
+            raise e
+    
+    def verification_steps(self,data):
+        """
+        Method that can be overwritten with the validation steps for differant schemas
+        """
+        self.fhir_resource_validate_schema(data)
+
+
+    def bulk_verify(self, entry_list):
+        """
+        Pass the 'entry' list from a bundle fhir object into entry_list
+
+        Args:
+            entry_list (list): list of fhir entities to validate
+        """
+
+        violations = 0
+        errors = []
+        for entry in entry_list:
+            try:
+                self.verification_steps(entry)
+            except ValidationError as e:
+                violations += 1
+                errors.append(e)
+        
+        if violations > 0:
+            print(f"Found {violations} validation errors!")
+            #Requires python 3.11+
+            raise ExceptionGroup("Validation Errors Summary", errors)
+
+class FHIRValueSetVerifier(BaseVerifier):
+    """
+        Class that uses pydantic to verify the FHIR schema as
+        pulled from the fhir.resources package. 
+
+        Is meant to be specific to ValueSet to validate nucc codes
+        as taken from the database. 
+    """
+    def __init__(self):
+        self.fhir_resource_type = ValueSet
+    
+    def verify_nucc_codes(self,data):
+        nucc_codes = get_nucc_codes_from_db()
+
+        for codeItem in data['compose']['include']:
+            #Check if code in ValueSet is part of nucc
+            if "nucc.org/provider-taxonomy" in codeItem['system']:
+                for codeValue in codeItem['concept']:
+                    try:
+                        assert codeValue['code'] in nucc_codes.keys()
+                    except AssertionError as e:
+                        print(f"Code {codeValue['code']} is not a valid nucc code!")
+                        raise ValidationError from e
+    
+    def verification_steps(self,data):
+        self.fhir_resource_validate_schema(data)
+        self.verify_nucc_codes(data)
