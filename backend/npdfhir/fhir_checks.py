c3263447990340b6f828564c6cc53aea14c58620
Isaac Milarsky <imilarsky@gmail.com>
2025-09-16 15:05:00 -0500

    Add luhn check, and make sure that we are using correct raising of ValidationError in pydantic
    Signed-off-by: Isaac Milarsky <imilarsky@gmail.com>

--- backend/npdfhir/fhir_checks.py
+++ backend/npdfhir/fhir_checks.py
@@ -1,7 +1,4 @@
-import traceback
 import re
-import requests
-import time
 from fhir.resources.valueset import ValueSet
 from fhir.resources.practitioner import Practitioner 
 from pydantic import ValidationError
@@ -32,6 +29,24 @@ def is_valid_npi_format(npi_value):
     digits_only = re.sub(r'\D', '', str(npi_value))
     npi_num = int(digits_only)
     return 999999999 <= npi_num <= 10000000000
+
+#NPI luhn algo defined here: 
+#https://build.fhir.org/ig/HL7/US-Core/StructureDefinition-us-core-practitioner.profile.json.html
+def npi_check_luhn_algorithm(npi_value):
+    def transform(d):
+        return d * 2 if d < 5 else d * 2 - 9
+    
+    digits_only = re.sub(r'\D', '', str(npi_value))
+
+    digits = [int(ch) for ch in digits_only[:10]]
+
+    total = sum(
+        transform(d) if i % 2 == 0 else d
+        for i, d in enumerate(digits)
+    ) + 24
+
+    return total % 10 == 0
+
 
 class BaseVerifier:
     """
@@ -93,15 +108,24 @@ class FHIRValueSetVerifier(BaseVerifier):
     def verify_codes(self,data):
         nucc_codes = get_nucc_codes_from_db()
 
-        for codeItem in data['compose']['include']:
+        for include_index, codeItem in enumerate(data['compose']['include']):
             #Check if code in ValueSet is part of nucc
             if "nucc.org/provider-taxonomy" in codeItem['system']:
-                for codeValue in codeItem['concept']:
+                for concept_index, codeValue in enumerate(codeItem['concept']):
                     try:
                         assert codeValue['code'] in nucc_codes.keys()
                     except AssertionError as e:
                         print(f"Code {codeValue['code']} is not a valid nucc code!")
-                        raise ValidationError from e
+                        raise ValidationError(
+                            [
+                                {
+                                    "type": "assertion_error",
+                                    "loc": ('compose','include',include_index,'concept', concept_index,'code',),
+                                    "msg": str(e)
+                                }
+                            ],
+                            model=self.fhir_resource_type,
+                        )
             elif "snomed.info/sct" in codeItem['system']:
                 raise NotImplementedError
             
@@ -115,7 +139,26 @@ class PractitionerVerifier(BaseVerifier):
     def __init__(self):
         self.fhir_resource_type = Practitioner
 
-    def verify_npi(self,npi):
+    def verify_npi(self,npi,position):
         if not is_valid_npi_format(npi):
-            raise ValueError(f"Invalid npi format for npi!: \n{npi}")
-
+            raise ValidationError(
+                [
+                    {
+                        "type": "value_error",
+                        "loc": ('identifier',position,'value',),
+                        "msg": "Npi value is not valid because of format!"
+                    }
+                ],
+                model=self.fhir_resource_type
+            )
+        if not npi_check_luhn_algorithm(npi):
+            raise ValidationError(
+                [
+                    {
+                        "type": "value_error",
+                        "loc": ('identifier',position,'value',),
+                        "msg": "Npi value is not valid because of luhn algo failing!"
+                    }
+                ],
+                model=self.fhir_resource_type
+            )
