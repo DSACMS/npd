da65b8df17b1750b93407f36f38eecaa739bca98
Blaine Price <william.price@cms.hhs.gov>
2025-10-01 11:55:50 -0400

    remove unused vpc id

--- infrastructure/nonprod/variables.tf
+++ infrastructure/nonprod/variables.tf
@@ -6,9 +6,5 @@ variable "tier" {
   default = "dev"
 }
 
-variable "vpc_id" {
-  default = "vpc-05bd23d8f48e88a4f"
-}
-
 variable "migration_image" { default = "public.ecr.aws/docker/library/hello-world:nanoserver-ltsc2022" }
 variable "fhir_api_image" { default = "public.ecr.aws/docker/library/hello-world:nanoserver-ltsc2022" }
