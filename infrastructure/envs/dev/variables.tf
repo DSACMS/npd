5936ae446730047b7b952f7646a411c5b9a375b4
Blaine Price <william.price@cms.hhs.gov>
2025-10-17 15:10:56 -0400

    Troubleshoot API Startup Failures
    - Troubleshoot networking configuration between load balancer and API
      security group
    - Troubleshoot API IAM permissions
    - Troubleshoot vpn security group permission

--- infrastructure/envs/dev/variables.tf
+++ infrastructure/envs/dev/variables.tf
@@ -6,5 +6,5 @@ variable "tier" {
   default = "dev"
 }
 
-variable "migration_image" { default = "public.ecr.aws/docker/library/hello-world:nanoserver-ltsc2022" }
-variable "fhir_api_image" { default = "public.ecr.aws/docker/library/hello-world:nanoserver-ltsc2022" }
+variable "migration_image" { default = "575012135727.dkr.ecr.us-east-1.amazonaws.com/npd-east-dev-fhir-api-migrations:latest" }
+variable "fhir_api_image" { default = "575012135727.dkr.ecr.us-east-1.amazonaws.com/npd-east-dev-fhir-api@sha256:d63a97d317feff204266a9655daa425377cab5832127e47076ca1ce91950d296" }
