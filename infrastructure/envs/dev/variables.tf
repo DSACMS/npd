6d725b5beffb96ba39041b122c74a31a77596fbb
Blaine Price <william.price@cms.hhs.gov>
2025-10-21 11:27:39 -0400

    revert variables to point to latest docker image

--- infrastructure/envs/dev/variables.tf
+++ infrastructure/envs/dev/variables.tf
@@ -7,4 +7,4 @@ variable "tier" {
 }
 
 variable "migration_image" { default = "575012135727.dkr.ecr.us-east-1.amazonaws.com/npd-east-dev-fhir-api-migrations:latest" }
-variable "fhir_api_image" { default = "575012135727.dkr.ecr.us-east-1.amazonaws.com/npd-east-dev-fhir-api@sha256:d63a97d317feff204266a9655daa425377cab5832127e47076ca1ce91950d296" }
+variable "fhir_api_image" { default = "575012135727.dkr.ecr.us-east-1.amazonaws.com/npd-east-dev-fhir-api:latest" }
