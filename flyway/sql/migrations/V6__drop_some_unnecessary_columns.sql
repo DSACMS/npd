a77c937f6f2a124a4dbe70833236979bd9a732be
PR0I <sarah.popelka1@cms.hhs.gov>
2025-10-22 16:26:53 -0400

    adding drop column statements

---
+++ flyway/sql/migrations/V6__drop_some_unnecessary_columns.sql
@@ -0,0 +1,5 @@
+ALTER TABLE npd.address DROP smarty_key;
+ALTER TABLE npd.address DROP barcode_delivery_code;
+ALTER TABLE npd.address_nonstandard DROP addressee;
+ALTER TABLE npd.address_us DROP addressee;
+ALTER TABLE npd.clinical_organization DROP endpoint_instance_id;
