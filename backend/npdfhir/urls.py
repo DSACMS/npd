ddf75c57282866cbc6691b0b39a7c0a9b601de39
PR0I <sarah.popelka1@cms.hhs.gov>
2025-10-24 08:30:24 -0400

    minor view naming tweak

--- backend/npdfhir/urls.py
+++ backend/npdfhir/urls.py
@@ -25,7 +25,7 @@ urlpatterns = [
     path("docs<format>", schema_view.without_ui(cache_timeout=0),
          name="schema-json"),
     re_path("docs/redoc/?", schema_view.with_ui("redoc",
-            cache_timeout=0), name="schema-swagger-ui"),
+            cache_timeout=0), name="schema-redoc-ui"),
     re_path("docs/?", schema_view.with_ui("swagger",
             cache_timeout=0), name="schema-swagger-ui"),
     path("healthCheck", views.health, name="healthCheck"),
