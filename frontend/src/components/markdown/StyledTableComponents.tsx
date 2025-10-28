62f3630b5d803b3bec325670b6b6756d756b675b
Sachin Panayil <sachinpanayil01@gmail.com>
2025-10-28 16:23:19 -0400

    refactored styling

--- frontend/src/components/markdown/StyledTableComponents.tsx
+++ frontend/src/components/markdown/StyledTableComponents.tsx
@@ -1,27 +1,10 @@
 import React from "react"
 
-const tableStyles = {
-  borderCollapse: "collapse" as const,
-  width: "100%",
-  border: "1px solid #ddd",
-}
-
-const cellStyles = {
-  border: "1px solid #ddd",
-  padding: "12px",
-}
-
-const headerStyles = {
-  ...cellStyles,
-  backgroundColor: "#f2f2f2",
-  textAlign: "left" as const,
-}
-
 export const StyledTable = (props: React.ComponentPropsWithoutRef<"table">) =>
-  React.createElement("table", { style: tableStyles, ...props })
+  React.createElement("table", { className: "ds-c-table", ...props })
 
 export const StyledTh = (props: React.ComponentPropsWithoutRef<"th">) =>
-  React.createElement("th", { style: headerStyles, ...props })
+  React.createElement("th", { className: "ds-c-table__header", ...props })
 
 export const StyledTd = (props: React.ComponentPropsWithoutRef<"td">) =>
-  React.createElement("td", { style: cellStyles, ...props })
+  React.createElement("td", { className: "ds-c-table__cell", ...props })
