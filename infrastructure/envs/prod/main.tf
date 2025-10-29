1b25a9aa70b63dfa3f3494b836aa9461824fab2a
Blaine Price <william.price@cms.hhs.gov>
2025-10-29 09:17:23 -0400

    increase database resources in prod to reflect dev

--- infrastructure/envs/prod/main.tf
+++ infrastructure/envs/prod/main.tf
@@ -54,7 +54,7 @@ module "api-db" {
   engine_version          = "17"
   family                  = "postgres17"
   instance_class          = "db.t3.micro"
-  allocated_storage       = 20
+  allocated_storage       = 100
   publicly_accessible     = false
   username                = "npd"
   db_name                 = "npd"
@@ -73,7 +73,7 @@ module "etl-db" {
   engine                  = "postgres"
   engine_version          = "17"
   family                  = "postgres17"
-  instance_class          = "db.t3.micro"
+  instance_class          = "db.t3.large"
   allocated_storage       = 100
   publicly_accessible     = false
   username                = "npd_etl"
