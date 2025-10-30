d48d29a88f76e32d416b103ffe9c8a96b57248a3
Adam Bachman <adam.bachman@cms.hhs.gov>
2025-10-30 10:30:13 -0400

    render sign in errors in the frontend

---
+++ frontend/src/hooks/useDjangoForm.ts
@@ -0,0 +1,30 @@
+// load form
+
+type DjangoFormErrors = {
+  [fieldName: string | "__all__"]: string[]
+}
+
+type DjangoForm = {
+  errors?: DjangoFormErrors
+}
+
+export const useDjangoForm = () => {
+  // if an element with id=form-errors exists, parse it as JSON
+  const form: DjangoForm = {}
+  const errorsEl = document.getElementById("form-errors")
+
+  if (errorsEl && errorsEl.textContent.startsWith("{")) {
+    let errors = null
+    try {
+      errors = JSON.parse(errorsEl.textContent)
+    } catch (e) {
+      console.error("error parsing form-errors JSON", e)
+    }
+
+    if (errors && Object.keys(errors).length) {
+      form.errors = errors
+    }
+  }
+
+  return form
+}
