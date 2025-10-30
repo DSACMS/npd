b9000326849b473df79932f86a91ef65e932e2b1
Blaine Price <william.price@cms.hhs.gov>
2025-10-30 13:00:01 -0400

    update prod configuration too

--- infrastructure/envs/prod/main.tf
+++ infrastructure/envs/prod/main.tf
@@ -84,6 +84,12 @@ module "etl-db" {
   db_subnet_group_name    = module.networking.private_subnet_group_name
   backup_retention_period = 7             # Remove automated snapshots after 7 days
   backup_window           = "03:00-04:00" # 11PM EST
+
+  parameters = [
+    # Parameters altered to enable DMS to perform database replication
+    { name = "rds.logical_replication", value = "1", apply_method = "pending-reboot" },
+    { name = "wal_sender_timeout", value = "0" }
+  ]
 }
 
 # ECS Cluster
@@ -107,13 +113,13 @@ module "ecs" {
 module "fhir-api" {
   source = "../../modules/fhir-api"
 
-  account_name               = local.account_name
-  fhir_api_migration_image   = var.migration_image
-  fhir_api_image             = var.fhir_api_image
-  redirect_to_strategy_page  = var.redirect_to_strategy_page
-  private_load_balancer      = var.fhir_api_private_load_balancer
-  ecs_cluster_id             = module.ecs.cluster_id
-  desired_task_count         = 3
+  account_name              = local.account_name
+  fhir_api_migration_image  = var.migration_image
+  fhir_api_image            = var.fhir_api_image
+  redirect_to_strategy_page = var.redirect_to_strategy_page
+  private_load_balancer     = var.fhir_api_private_load_balancer
+  ecs_cluster_id            = module.ecs.cluster_id
+  desired_task_count        = 3
   db = {
     db_instance_master_user_secret_arn = module.api-db.db_instance_master_user_secret_arn
     db_instance_address                = module.api-db.db_instance_address
@@ -133,11 +139,11 @@ module "fhir-api" {
 module "etl" {
   source = "../../modules/etl"
 
-  account_name   = local.account_name
-  dagster_image  = var.dagster_image
+  account_name             = local.account_name
+  dagster_image            = var.dagster_image
   fhir_api_migration_image = var.migration_image
-  ecs_cluster_id = module.ecs.cluster_id
-  npd_sync_task_arn = "*"
+  ecs_cluster_id           = module.ecs.cluster_id
+  npd_sync_task_arn        = "*"
   db = {
     db_instance_master_user_secret_arn = module.etl-db.db_instance_master_user_secret_arn
     db_instance_address                = module.etl-db.db_instance_address
