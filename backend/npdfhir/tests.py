5c1f895901b03162303b7d93c1f5b4a78f89d80e
Isaac Milarsky <imilarsky@gmail.com>
2025-11-03 10:43:35 -0600

    Add Missing Test for PractitionerRoleViewSetTestCase (#167)
    * add sorting test for practitionerroleviewset
    
    Signed-off-by: Isaac Milarsky <imilarsky@gmail.com>
    
    * Apply suggestion from @abachman-dsac
    
    Co-authored-by: Adam Bachman <adam.bachman@cms.hhs.gov>
    
    ---------
    
    Signed-off-by: Isaac Milarsky <imilarsky@gmail.com>
    Co-authored-by: Adam Bachman <adam.bachman@cms.hhs.gov>

--- backend/npdfhir/tests.py
+++ backend/npdfhir/tests.py
@@ -579,6 +579,51 @@ class PractitionerRoleViewSetTestCase(APITestCase):
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertEqual(response["Content-Type"], "application/fhir+json")
         self.assertIn("results", response.data)
+    
+    def test_list_in_proper_order(self):
+        url = reverse("fhir-practitionerrole-list")
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response["Content-Type"], "application/fhir+json")
+
+        # Extract ids
+        ids = [
+            d['resource'].get('id', {})
+            for d in response.data["results"]["entry"]
+        ]
+
+
+        #Corresponds to the following location name order
+        """
+        A BEAUTIFUL SMILE DENTISTRY, L.L.C.
+        ADIRONDACK MEDICAL HEALTH CARE ASSOCIATES PLLC
+        ADIRONDACK MEDICAL HEALTH CARE ASSOCIATES PLLC
+        ADIRONDACK MEDICAL HEALTH CARE ASSOCIATES PLLC
+        ADIRONDACK MEDICAL HEALTH CARE ASSOCIATES PLLC
+        ADIRONDACK MEDICAL HEALTH CARE ASSOCIATES PLLC
+        ADR LLC
+        ADR LLC
+        ADR LLC
+        ADR LLC
+        """
+
+        sorted_ids = [
+            'e9554c87-6e4e-4df6-93fb-88ee4bc4e5be',
+            '9f50dfd8-098a-4e6d-a4ad-ded2175a5321',
+            '90011f74-1c0d-4461-95b5-cb346cdbc64b',
+            '874c25e0-44fd-48e9-832a-a80f1d07491a',
+            '0ba12b55-05e1-450f-8a2c-454a93425a34',
+            '38eac005-9373-44ab-bf5d-57b84bca7cb4',
+            'cd3fe6b7-02b0-4136-8db8-4c3867aab131',
+            '093091b7-aba7-4acb-8338-65996de10813',
+            '2e18cd31-4a89-475b-82be-71ad75011713',
+            '59ef9dd6-60e8-4a64-a52c-6f44c540184f'
+        ]
+        
+
+        self.assertEqual(
+            ids, sorted_ids, f"Expected Practitioner roles sorted by order of location name but got {ids}\n Sorted: {sorted_ids}")
+
 
     def test_list_with_custom_page_size(self):
         url = reverse("fhir-practitionerrole-list")
