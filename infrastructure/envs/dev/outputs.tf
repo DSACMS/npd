d86f713f06873fec52f1f34c02430017db8c9aef
Blaine Price <william.price@cms.hhs.gov>
2025-11-12 14:46:35 -0500

    update outputs

--- infrastructure/envs/dev/outputs.tf
+++ infrastructure/envs/dev/outputs.tf
@@ -2,6 +2,10 @@ output "api_alb_dns_name" {
   value = module.fhir-api.api_alb_dns_name
 }
 
+output "api_dot_alb_dns_name" {
+  value = module.fhir-api.api_dot_alb_dns_name
+}
+
 output "api_db_instance_endpoint" {
   value = module.api-db.db_instance_endpoint
 }
