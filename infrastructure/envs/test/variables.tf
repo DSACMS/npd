2743fbbcc40ff441624302789dfbadd740412f3a
Blaine Price <william.price@cms.hhs.gov>
2025-11-17 17:04:28 -0500

    update default images for services

--- infrastructure/envs/test/variables.tf
+++ infrastructure/envs/test/variables.tf
@@ -6,8 +6,8 @@ variable "tier" {
   default = "test"
 }
 
-variable "migration_image" { default = "596240962403.dkr.ecr.us-east-1.amazonaws.com/npd-east-test-fhir-api-migrations:latest" }
-variable "fhir_api_image" { default = "596240962403.dkr.ecr.us-east-1.amazonaws.com/npd-east-test-fhir-api:latest" }
-variable "dagster_image" { default = "596240962403.dkr.ecr.us-east-1.amazonaws.com/npd-east-test-dagster:latest" }
+variable "migration_image" { default = "575012135727.dkr.ecr.us-east-1.amazonaws.com/npd-east-test-fhir-api-migrations:latest" }
+variable "fhir_api_image" { default = "575012135727.dkr.ecr.us-east-1.amazonaws.com/npd-east-test-fhir-api:latest" }
+variable "dagster_image" { default = "575012135727.dkr.ecr.us-east-1.amazonaws.com/npd-east-test-dagster:latest" }
 variable "fhir_api_private_load_balancer" { default = true }
 variable "require_authentication" { default = "True" }
