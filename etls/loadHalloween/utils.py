beb1d9e88f6957bcccb4542ceeaa9c4959b103db
PR0I <sarah.popelka1@cms.hhs.gov>
2025-11-26 17:27:50 -0500

    tweaking credentials

--- etls/loadHalloween/utils.py
+++
@@ -1,7 +0,0 @@
-def show_or_load(df, table_name, schema_name, engine, load=False):
-    if load:
-        print(f'Loading {table_name}')
-        df.to_sql(table_name, schema=schema_name, con=engine,
-                  if_exists='append', index=False)
-    else:
-        print(df.head())
