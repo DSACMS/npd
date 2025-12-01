545c8150d5cd3d7cfb83e1e4067939ffcc985ff9
Adam Bachman <adam.bachman@cms.hhs.gov>
2025-12-01 14:12:52 -0500

    [NDH-398] Organization details screen - initial work (#198)
    * [NDH-398] introduce @tanstack/react-query, start shaping the Organization lookup page
    
    * ensure Organization page renders in test with feature flags obeyed
    
    * fix inverse feature flag logic
    
    * npm install inside container keeps fiddling with package-lock.json
    
    * set VITE_API_BASE_URL in GitHub Action frontend test workflow
    
    * add basic Organization page sections, update styles
    
    * put update your details alert inside feature flag block

--- frontend/src/pages/Organization/Organization.tsx
+++ frontend/src/pages/Organization/Organization.tsx
@@ -1,22 +1,40 @@
 import { Alert } from "@cmsgov/design-system"
 import classNames from "classnames"
+import { useParams } from "react-router"
 import { FeatureFlag } from "../../components/FeatureFlag"
+import { LoadingIndicator } from "../../components/LoadingIndicator"
+import {
+  organizationNpiSelector,
+  useOrganizationAPI,
+} from "../../state/requests/organizations"
 import layout from "../Layout.module.css"
 
 export const Organization = () => {
+  const { organizationId } = useParams()
+  const { data, isLoading } = useOrganizationAPI(organizationId)
+
+  if (isLoading) {
+    return <LoadingIndicator />
+  }
+
   const contentClass = classNames(layout.content, "ds-l-container")
+  const bannerClass = classNames(layout.banner)
 
   return (
     <>
-      <section className="ds-l-container">
-        <div className="ds-l-row">
-          <div className="ds-l-col--12">
-            <div className={layout.leader}>
-              <span className={layout.subtitle}>Provider group</span>
-              <div role="heading" aria-level={1} className={layout.title}>
-                Lorem Mental Health Group LLC
+      <section className={bannerClass}>
+        <div className="ds-l-container">
+          <div className="ds-l-row">
+            <div className="ds-l-col--12">
+              <div className={layout.leader}>
+                <span className={layout.subtitle}>Provider group</span>
+                <div role="heading" aria-level={1} className={layout.title}>
+                  {data?.name}
+                </div>
+                <span className={layout.subtitle}>
+                  NPI: {organizationNpiSelector(data)}
+                </span>
               </div>
-              <span className={layout.subtitle}>NPI: 1234567891</span>
             </div>
           </div>
         </div>
@@ -29,10 +47,47 @@ export const Organization = () => {
         </FeatureFlag>
 
         <FeatureFlag name="ORGANIZATION_LOOKUP_DETAILS">
-          <p className="ds-u-margin-top--7 ds-u-margin-bottom--2">
-            Details go here.
-          </p>
+          <Alert heading="Are you the practitioner listed?">
+            Learn how to <a href="#">update your information</a>.
+          </Alert>
+
+          <section className={layout.section}>
+            <h2>About</h2>
+            <p>[demographic information]</p>
+          </section>
+
+          <section className={layout.section}>
+            <h2>Contact information</h2>
+            <p>[contact information]</p>
+          </section>
+
+          <section className={layout.section}>
+            <h2>Identifiers</h2>
+            <p>[identifier information]</p>
+          </section>
+
+          <section className={layout.section}>
+            <h2>Taxonomy</h2>
+            <p>[taxonomy information]</p>
+          </section>
+
+          <section className={layout.section}>
+            <h2>Endpoints</h2>
+            <p>[endpoint information]</p>
+          </section>
+
+          <section className={layout.section}>
+            <h2>Locations</h2>
+            <p>[location information]</p>
+          </section>
+
+          <section className={layout.section}>
+            <h2>Practitioners</h2>
+            <p>[practitioner information]</p>
+          </section>
         </FeatureFlag>
+
+        <div className="ds-u-margin-top--7"></div>
       </main>
     </>
   )
