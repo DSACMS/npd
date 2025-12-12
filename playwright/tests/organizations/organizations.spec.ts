cb4f0ff705c0fc4a052df7f75a38a45b1b43c9e6
Adam Bachman <adam.bachman@cms.hhs.gov>
2025-12-12 14:40:08 -0500

    make e2e organizations tests rely on data present in system

--- playwright/tests/organizations/organizations.spec.ts
+++ playwright/tests/organizations/organizations.spec.ts
@@ -1,34 +1,39 @@
 import { expect, test } from "@playwright/test"
 
-import fhir_organization from "../../../frontend/tests/fixtures/fhir_organization.json"
-import fhir_organizations from "../../../frontend/tests/fixtures/fhir_organizations.json"
+let organization: { npi: string; id: string; name: string } = {
+  npi: "UNSET",
+  id: "UNSET",
+  name: "UNSET",
+}
+
+// load an organization record from the API before running tests
+test.beforeAll(async ({ request }) => {
+  // expects a FhirCollection<FhirOrganization> API response
+  const response = await request.get(
+    "/fhir/Organization/?identifier=1234567893",
+  )
+  const payload = await response.json()
+
+  const resource = payload.results.entry[0].resource
+
+  organization = {
+    id: resource.id,
+    name: resource.name,
+    npi: resource.identifier[0].value,
+  }
+
+  // it should look like the /fhir/Organization/ record created by seedsystem
+  expect(organization).toMatchObject(
+    expect.objectContaining({
+      id: expect.stringMatching(/[\d-]+/),
+      name: "Test Org",
+      npi: "1234567893",
+    }),
+  )
+})
 
 test.describe("Organizations", () => {
-  test("visit an Organization page", async ({ page }) => {
-    // mock the API using the existing test fixture from frontend/
-    await page.route("*/**/fhir/Organization/12345/", async (route) => {
-      const json = fhir_organization
-      await route.fulfill({ json })
-    })
-
-    await page.goto("/organizations/12345")
-
-    await expect(page).toHaveURL("/organizations/12345")
-
-    await expect(page.locator("div[role='heading']")).toContainText(
-      "Acme Healthcare System",
-    )
-  })
-
   test("visit the Organizations listing page", async ({ page }) => {
-    await page.route(
-      new RegExp(".*/fhir/Organization/\\?.*"),
-      async (route) => {
-        const json = fhir_organizations
-        await route.fulfill({ json })
-      },
-    )
-
     await page.goto("/organizations")
 
     await expect(page).toHaveURL("/organizations")
@@ -37,6 +42,25 @@ test.describe("Organizations", () => {
       "All Organizations",
     )
 
-    await expect(page.getByText("NPI: 1679576367")).toBeInViewport()
+    await expect(page.getByText(`NPI: ${organization?.npi}`)).toBeVisible()
+  })
+
+  test("visit an Organization page", async ({ page }) => {
+    // visit listing page
+    await page.goto("/organizations")
+
+    // pick known organization
+    await page.getByText(organization.name).click()
+
+    // should be on the single organization show page
+    await expect(page).toHaveURL(`/organizations/${organization.id}`)
+    await expect(page.locator("div[role='heading']")).toContainText(
+      organization.name,
+    )
+
+    const banner = page.locator("section.banner")
+    await expect(banner.getByText("Provider group")).toBeVisible()
+    await expect(banner.getByText(organization.name)).toBeVisible()
+    await expect(banner.getByText(`NPI: ${organization.npi}`)).toBeVisible()
   })
 })
