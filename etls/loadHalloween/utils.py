e1256a4e045c671f2372857fde5432ef69b5d2d9
PR0I <sarah.popelka1@cms.hhs.gov>
2025-12-15 16:47:44 -0500

    some tweaks for ids

--- etls/loadHalloween/utils.py
+++ etls/loadHalloween/utils.py
@@ -6,11 +6,12 @@ from sqlalchemy import create_engine
 def createEngine():
     # Get database details and create engine
     load_dotenv()
-    username = os.getenv('SOURCE_DB_USER')
-    password = os.getenv('SOURCE_DB_PASSWORD')
-    instance = os.getenv('SOURCE_DB_HOST')
-    db = os.getenv('SOURCE_DB_NAME')
-    port = os.getenv('SOURCE_DB_PORT')
+    username = os.getenv("SOURCE_DB_USER")
+    password = os.getenv("SOURCE_DB_PASSWORD")
+    instance = os.getenv("SOURCE_DB_HOST")
+    db = os.getenv("SOURCE_DB_NAME")
+    port = os.getenv("SOURCE_DB_PORT")
     engine = create_engine(
-        f"postgresql+psycopg2://{username}:{password}@{instance}:{port}/{db}")
+        f"postgresql+psycopg2://{username}:{password}@{instance}:{port}/{db}"
+    )
     return engine
