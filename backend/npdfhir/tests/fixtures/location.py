94072ed316b4b4feef2966da0f1597ba9c1ca7d8
Isaac Milarsky <imilarsky@gmail.com>
2025-12-19 13:08:44 -0600

    patch location fixture
    Signed-off-by: Isaac Milarsky <imilarsky@gmail.com>

--- backend/npdfhir/tests/fixtures/location.py
+++ backend/npdfhir/tests/fixtures/location.py
@@ -1,4 +1,5 @@
 import uuid
+import random
 
 from ...models import Address, AddressUs, FipsState, Location
 from .organization import create_organization
@@ -19,7 +20,7 @@ def create_location(
 
     fips_code = FipsState.objects.get(abbreviation=state)
     addr_us = AddressUs.objects.create(
-        id=str(uuid.uuid4())[:10],
+        id=random.randint(-100000000000, 100000000000),
         delivery_line_1=addr_line_1,
         city_name=city,
         state_code_id=fips_code.id,
