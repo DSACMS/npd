71e195d497b199280aef71d49d0158e94199e659
Blaine Price <william.price@cms.hhs.gov>
2025-12-31 11:11:15 -0500

    remove infrastructure definitions

--- infrastructure/modules/dns/variables.tf
+++
@@ -1,9 +0,0 @@
-variable "api_domain" {}
-variable "api_alb_dns_name" {}
-variable "directory_domain" {}
-variable "directory_alb_dns_name" {}
-variable "directory_alb_zone_id" {}
-variable "etl_domain" {}
-variable "etl_alb_dns_name" {}
-variable "namespace_domain" {}
-variable "enable_internal_domain_for_directory" {}
--- infrastructure/modules/etl/outputs.tf
+++
@@ -1,3 +0,0 @@
-output "etl_ui_alb_dns_name" {
-  value = aws_lb.etl_ui_alb.dns_name
-}
