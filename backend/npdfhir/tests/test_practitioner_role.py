5f32bed755c406ac0ec6f4f37dbcd1c0555a554e
Isaac Milarsky <imilarsky@gmail.com>
2026-01-06 13:51:52 -0600

    lint
    Signed-off-by: Isaac Milarsky <imilarsky@gmail.com>

--- backend/npdfhir/tests/test_practitioner_role.py
+++ backend/npdfhir/tests/test_practitioner_role.py
@@ -4,7 +4,6 @@ from django.urls import reverse
 from rest_framework import status
 
 from .api_test_case import APITestCase
-from .fixtures.practitioner import create_full_practitionerrole
 from .helpers import (
     assert_fhir_response,
     assert_has_results,
