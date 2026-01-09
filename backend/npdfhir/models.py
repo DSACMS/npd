fda119a20df7ceed881d10486160cf8395a431dc
PR0I <sarah.popelka1@cms.hhs.gov>
2026-01-09 11:31:56 -0500

    cleaning up models.py

--- backend/npdfhir/models.py
+++ backend/npdfhir/models.py
@@ -135,75 +135,6 @@ class AddressUs(models.Model):
         db_table = "address_us"
 
 
-class AuthGroup(models.Model):
-    name = models.CharField(unique=True, max_length=150)
-
-    class Meta:
-        managed = False
-        db_table = "auth_group"
-
-
-class AuthGroupPermissions(models.Model):
-    id = models.BigAutoField(primary_key=True)
-    group = models.ForeignKey(AuthGroup, models.DO_NOTHING)
-    permission = models.ForeignKey("AuthPermission", models.DO_NOTHING)
-
-    class Meta:
-        managed = False
-        db_table = "auth_group_permissions"
-        unique_together = (("group", "permission"),)
-
-
-class AuthPermission(models.Model):
-    name = models.CharField(max_length=255)
-    content_type = models.ForeignKey("DjangoContentType", models.DO_NOTHING)
-    codename = models.CharField(max_length=100)
-
-    class Meta:
-        managed = False
-        db_table = "auth_permission"
-        unique_together = (("content_type", "codename"),)
-
-
-class AuthUser(models.Model):
-    password = models.CharField(max_length=128)
-    last_login = models.DateTimeField(blank=True, null=True)
-    is_superuser = models.BooleanField()
-    username = models.CharField(unique=True, max_length=150)
-    first_name = models.CharField(max_length=150)
-    last_name = models.CharField(max_length=150)
-    email = models.CharField(max_length=254)
-    is_staff = models.BooleanField()
-    is_active = models.BooleanField()
-    date_joined = models.DateTimeField()
-
-    class Meta:
-        managed = False
-        db_table = "auth_user"
-
-
-class AuthUserGroups(models.Model):
-    id = models.BigAutoField(primary_key=True)
-    user = models.ForeignKey(AuthUser, models.DO_NOTHING)
-    group = models.ForeignKey(AuthGroup, models.DO_NOTHING)
-
-    class Meta:
-        managed = False
-        db_table = "auth_user_groups"
-        unique_together = (("user", "group"),)
-
-
-class AuthUserUserPermissions(models.Model):
-    id = models.BigAutoField(primary_key=True)
-    user = models.ForeignKey(AuthUser, models.DO_NOTHING)
-    permission = models.ForeignKey(AuthPermission, models.DO_NOTHING)
-
-    class Meta:
-        managed = False
-        db_table = "auth_user_user_permissions"
-        unique_together = (("user", "permission"),)
-
-
 class ClinicalOrganization(models.Model):
     organization = models.OneToOneField("Organization", models.DO_NOTHING, blank=True, null=True)
     npi = models.OneToOneField("Npi", models.DO_NOTHING, db_column="npi", primary_key=True)
@@ -227,50 +158,6 @@ class DegreeType(models.Model):
     class Meta:
         managed = False
         db_table = "degree_type"
-
-
-class DjangoAdminLog(models.Model):
-    action_time = models.DateTimeField()
-    object_id = models.TextField(blank=True, null=True)
-    object_repr = models.CharField(max_length=200)
-    action_flag = models.SmallIntegerField()
-    change_message = models.TextField()
-    content_type = models.ForeignKey("DjangoContentType", models.DO_NOTHING, blank=True, null=True)
-    user = models.ForeignKey(AuthUser, models.DO_NOTHING)
-
-    class Meta:
-        managed = False
-        db_table = "django_admin_log"
-
-
-class DjangoContentType(models.Model):
-    app_label = models.CharField(max_length=100)
-    model = models.CharField(max_length=100)
-
-    class Meta:
-        managed = False
-        db_table = "django_content_type"
-        unique_together = (("app_label", "model"),)
-
-
-class DjangoMigrations(models.Model):
-    app = models.CharField(max_length=255)
-    name = models.CharField(max_length=255)
-    applied = models.DateTimeField()
-
-    class Meta:
-        managed = False
-        db_table = "django_migrations"
-
-
-class DjangoSession(models.Model):
-    session_key = models.CharField(primary_key=True, max_length=40)
-    session_data = models.TextField()
-    expire_date = models.DateTimeField()
-
-    class Meta:
-        managed = False
-        db_table = "django_session"
 
 
 class EhrVendor(models.Model):
@@ -352,21 +239,6 @@ class EndpointInstanceType(models.Model):
     class Meta:
         managed = False
         db_table = "endpoint_instance_type"
-
-
-class EndpointTmp(models.Model):
-    organization_name = models.CharField(max_length=1000, blank=True, null=True)
-    identifier_type = models.CharField(max_length=100, blank=True, null=True)
-    identifier_value = models.BigIntegerField(blank=True, null=True)
-    address = models.CharField(max_length=1000, blank=True, null=True)
-    fhir_endpoint_address = models.CharField(max_length=1000, blank=True, null=True)
-    fhir_version = models.CharField(max_length=100, blank=True, null=True)
-    vendor_name = models.CharField(max_length=100, blank=True, null=True)
-    organization_id = models.UUIDField(blank=True, null=True)
-
-    class Meta:
-        managed = False
-        db_table = "endpoint_tmp"
 
 
 class EndpointToOtherId(models.Model):
@@ -481,23 +353,6 @@ class FlagsFlagstate(models.Model):
         managed = False
         db_table = "flags_flagstate"
         unique_together = (("name", "condition", "value"),)
-
-
-class FlywaySchemaHistory(models.Model):
-    installed_rank = models.IntegerField(primary_key=True)
-    version = models.CharField(max_length=50, blank=True, null=True)
-    description = models.CharField(max_length=200)
-    type = models.CharField(max_length=20)
-    script = models.CharField(max_length=1000)
-    checksum = models.IntegerField(blank=True, null=True)
-    installed_by = models.CharField(max_length=100)
-    installed_on = models.DateTimeField()
-    execution_time = models.IntegerField()
-    success = models.BooleanField()
-
-    class Meta:
-        managed = False
-        db_table = "flyway_schema_history"
 
 
 class Individual(models.Model):
