5b26ce6a520b6b7b258e9e77f9441fb6b15c5b47
Sachin Panayil <sachinpanayil01@gmail.com>
2026-01-15 12:02:38 -0500

    removing remanining filters

--- backend/npdfhir/views.py
+++ backend/npdfhir/views.py
@@ -11,7 +11,7 @@ from rest_framework import viewsets
 from rest_framework.views import APIView
 from rest_framework.renderers import BrowsableAPIRenderer
 from rest_framework.response import Response
-from rest_framework.filters import SearchFilter, OrderingFilter
+from rest_framework.filters import OrderingFilter
 
 from .pagination import CustomPaginator
 from .renderers import FHIRRenderer
@@ -67,7 +67,7 @@ class FHIREndpointViewSet(viewsets.GenericViewSet):
         renderer_classes = [FHIRRenderer, BrowsableAPIRenderer]
     else:
         renderer_classes = [FHIRRenderer]
-    filter_backends = [DjangoFilterBackend, SearchFilter, ParamOrderingFilter]
+    filter_backends = [DjangoFilterBackend, ParamOrderingFilter]
     filterset_class = EndpointFilterSet
     ordering_fields = ["name", "address", "ehr_vendor_name"]
     pagination_class = CustomPaginator
