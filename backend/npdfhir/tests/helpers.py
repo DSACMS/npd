bb7b4a3a70d84e11da70e782c2783c28dc3c291d
Isaac Milarsky <imilarsky@gmail.com>
2026-01-22 14:33:49 -0600

    [NDH-677] Improve Test Assertions for Location Tests (#314)
    * NDH-677 clean up address test assertions and fix issue where address use was not present in serialized location data. Also, add address use data in fixture methods for location
    
    Signed-off-by: Isaac Milarsky <imilarsky@gmail.com>
    
    * patch org filter by org type and create test for filter set method
    
    Signed-off-by: Isaac Milarsky <imilarsky@gmail.com>
    
    * Fix address use filter
    
    Signed-off-by: Isaac Milarsky <imilarsky@gmail.com>
    
    * NDH-677 Improve helper method to check if there are results and patch tests that improperly didn't have results
    
    Signed-off-by: Isaac Milarsky <imilarsky@gmail.com>
    
    * revise location filter set as well as add tests
    
    Signed-off-by: Isaac Milarsky <imilarsky@gmail.com>
    
    * lint
    
    Signed-off-by: Isaac Milarsky <imilarsky@gmail.com>
    
    ---------
    
    Signed-off-by: Isaac Milarsky <imilarsky@gmail.com>

--- backend/npdfhir/tests/helpers.py
+++ backend/npdfhir/tests/helpers.py
@@ -28,6 +28,7 @@ def assert_fhir_response(test_case, response, expected_status=200):
 
 def assert_has_results(test_case, response):
     test_case.assertIn("results", response.data)
+    test_case.assertGreater(len(response.data["results"]["entry"]),0)
 
 
 def assert_pagination_limit(test_case, response, max_size=100):
@@ -49,3 +50,15 @@ def extract_practitioner_names(response):
 
 def extract_resource_ids(response):
     return [d["resource"].get("id", {}) for d in response.data["results"]["entry"]]
+
+def concat_address_string(address):
+    address_string = ""
+
+    for line in address["line"]:
+        address_string += line + " "
+    
+    address_string += address["city"] + " "
+    address_string += address["state"] + " "
+    address_string += address["postalCode"]
+
+    return address_string
