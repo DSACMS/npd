097f9e06ebe3d91e11a091c79b3ba3f257237f45
PR0I <sarah.popelka1@cms.hhs.gov>
2026-01-22 12:52:34 -0500

    I should have been using the apiSchema var whoops

--- flyway/sql/migrations/V16__add_geometry_column.sql
+++ flyway/sql/migrations/V16__add_geometry_column.sql
@@ -1,9 +1,9 @@
 create extension postgis;
 
-alter table npd.address_us add column geolocation geometry;
-alter table npd.address_international add column geolocation geometry;
-alter table npd.address_nonstandard add column geolocation geometry;
+alter table ${apiSchema}.address_us add column geolocation geometry;
+alter table ${apiSchema}.address_international add column geolocation geometry;
+alter table ${apiSchema}.address_nonstandard add column geolocation geometry;
 
-update npd.address_us set geolocation = ST_point(longitude, latitude);
-update npd.address_international set geolocation = ST_point(longitude, latitude);
-update npd.address_nonstandard set geolocation = ST_point(longitude, latitude);
+update ${apiSchema}.address_us set geolocation = ST_point(longitude, latitude);
+update ${apiSchema}.address_international set geolocation = ST_point(longitude, latitude);
+update ${apiSchema}.address_nonstandard set geolocation = ST_point(longitude, latitude);
