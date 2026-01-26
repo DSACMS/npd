c8d9f23fd7cbb00f4431ded507e7fb071c1e7b08
PR0I <sarah.popelka1@cms.hhs.gov>
2026-01-26 08:40:30 -0500

    changing endpoint.status field from varchar(64) to an enum

--- flyway/sql/migrations/V16__add_endpoint_status_column.sql
+++ flyway/sql/migrations/V16__add_endpoint_status_column.sql
@@ -1,1 +1,2 @@
-alter table npd.endpoint_instance add status varchar(64);
+CREATE TYPE endpoint_status AS ENUM ('active', 'limited', 'suspended', 'error', 'off', 'entered-in-error');
+alter table npd.endpoint_instance add status endpoint_status DEFAULT 'active';
