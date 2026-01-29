a9e5619a79858282ee48b635e9442717c000f64b
Blaine Price <william.price@cms.hhs.gov>
2026-01-29 13:58:20 -0500

    I do believe it is working

--- backend/app/settings.py
+++ backend/app/settings.py
@@ -220,9 +220,8 @@ STATICFILES_DIRS = [
 # STATICFILES_DIRS = [
 #        os.path.join(BASE_DIR, "static"),
 #    ]
-
 # This is used with the static asset delivery middleware "WhiteNoise"
-STATIC_ROOT = os.path.join(BASE_DIR, "static", "assets")
+STATIC_ROOT = BASE_DIR / "staticfiles"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
